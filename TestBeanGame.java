082cd535e687fff8c1b8b264fdaae2b5b2bb04e5
U10416027 <U10416027@go.utaipei.edu.tw>
2016-03-20 19:38:08 +0800

    Update TestBeanGame.java

--- TestBeanGame.java
+++ TestBeanGame.java
@@ -1,1 +1,8 @@
+import javafx.application.Application;
 
+public class TestBeanGame{
+	public static void main(String[] args){
+		//call the BeanGame class
+		Application.launch(BeanGame.class, args);
+	}
+}
